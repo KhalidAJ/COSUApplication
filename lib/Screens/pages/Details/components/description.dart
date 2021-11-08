66665e52f05511de8c3a2d5c219a5f2058530856
Khalid Al Ajlan <khalidhh85@gmail.com>
2021-11-08 21:57:53 +0300

    Added All Browse COSU necessities

---
+++ lib/Screens/pages/Details/components/description.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:cosu_app/Screens/Models/Product.dart';
+
+import 'package:cosu_app/Screens/pages/BrowseCOSU/BrowseConstants.dart';
+
+class Description extends StatelessWidget {
+  const Description({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin),
+      child: Text(
+        product.description,
+        style: TextStyle(height: 1.5),
+      ),
+    );
+  }
+}
